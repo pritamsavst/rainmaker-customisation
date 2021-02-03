6082960adb1a539d5bde702d2bc6dcf706295589
Extrapreneurs <60169213+extrapre@users.noreply.github.com>
2021-02-03 16:54:10 +0530

    OPMS RoadCut NOC Localization

---
+++ configs/egov-localization/rainmaker-pm-services.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby)
+( '42e17862-dbd8-4pm6-95f6-382ec3e00030','en_IN', 'PM_ASSIGNEE','Assignee','ch','rainmaker-pm', '94');
