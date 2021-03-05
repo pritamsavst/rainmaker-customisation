6272955e0a3895fede26ee9b1dcd80e0de598085
Extrapreneurs <60169213+extrapre@users.noreply.github.com>
2021-03-05 18:00:00 +0530

    Echallan Mark as paid and delete challan localization updated

--- configs/egov-localization/rainmaker-ec.sql
+++ configs/egov-localization/rainmaker-ec.sql
@@ -1,6 +1,20 @@
 DELETE FROM public.message WHERE code = 'EC_SEND_NOTIFICATION' and module='rainmaker-ec' and tenantId='ch';
 DELETE FROM public.message WHERE code = 'EC_SEND_MESSAGE_BUTTON' and module='rainmaker-ec' and tenantId='ch';
+
+DELETE FROM public.message WHERE code = 'EC_MARK_AS_PAID_BUTTON' and module='rainmaker-ec' and tenantId='ch';
+DELETE FROM public.message WHERE code = 'EC_MARK_AS_PAID_SUCCESS' and module='rainmaker-ec' and tenantId='ch';
+DELETE FROM public.message WHERE code = 'EC_DELETE_CHALLAN_BUTTON' and module='rainmaker-ec' and tenantId='ch';
+DELETE FROM public.message WHERE code = 'EC_DELETE_CHALLAN_SUCCESS' and module='rainmaker-ec' and tenantId='ch';
+
 INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
 ( '52e17862-dbd8-4ec6-95f6-382ec3e00001','en_IN', 'EC_SEND_NOTIFICATION','Notification Sent Successfully !','ch','rainmaker-hc', '94');
 INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
 ( '52e17862-dbd8-4ec6-95f6-382ec3e00002','en_IN', 'EC_SEND_MESSAGE_BUTTON','Send Message','ch','rainmaker-hc', '94');
+INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
+( '52e17862-dbd8-4ec6-95f6-382ec3e00003','en_IN', 'EC_MARK_AS_PAID_BUTTON','Mark As Paid','ch','rainmaker-hc', '94');
+INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
+( '52e17862-dbd8-4ec6-95f6-382ec3e00004','en_IN', 'EC_MARK_AS_PAID_SUCCESS','Marked Challan As Paid Successfully.','ch','rainmaker-hc', '94');
+INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
+( '52e17862-dbd8-4ec6-95f6-382ec3e00005','en_IN', 'EC_DELETE_CHALLAN_BUTTON','Delete Challan','ch','rainmaker-hc', '94');
+INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
+( '52e17862-dbd8-4ec6-95f6-382ec3e00006','en_IN', 'EC_DELETE_CHALLAN_SUCCESS','Challan Deleted','ch','rainmaker-hc', '94');
