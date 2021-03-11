2cb8ffbbf35ae0adf5078f9c330861061ca4543b
pritamsavst <57742399+pritamsavst@users.noreply.github.com>
2021-03-11 14:24:40 +0530

    Create localization_pgr_new_10032021.sql

---
+++ configs/egov-localization/localization_pgr_new_10032021.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO message (id,locale,code,message,tenantid,"module",createdby,createddate,lastmodifiedby,lastmodifieddate) VALUES
+('4d3b6355-1abb-5618-ac6f-2bc063b75064e','en_IN','CS_ADDCOMPLAINT_COMPLAINT_DETAILS_VALIDATION','Enter valid complaint additional details','ch','rainmaker-pgr',24226,now(),NULL,NULL)
+,('4d3b6355-1abb-5619-ac6f-2bc063b75064e','en_IN','CS_ADDCOMPLAINT_LANDMARK_VALIDATION','Enter valid landmark','ch','rainmaker-pgr',24226,now(),NULL,NULL)
+,('4d3b6355-1abb-5620-ac6f-2bc063b75064e','en_IN','CS_HOUSE_NO_ERROR_MESSAGE_VALIDATION','Enter valid House no','ch','rainmaker-pgr',24226,now(),NULL,NULL)
+,('4d3b6355-1abb-5621-ac6f-2bc063b75064e','en_IN','CORE_COMMON_INVALID_FILE_EXTENSION','Please select valid file!','ch','rainmaker-common',24226,now(),NULL,NULL);
