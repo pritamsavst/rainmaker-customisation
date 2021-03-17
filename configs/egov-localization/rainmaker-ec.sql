370ac7d155f6199937ba75c96349f5232f8654e5
Extrapreneurs <60169213+extrapre@users.noreply.github.com>
2021-03-17 17:47:26 +0530

    Echallan Update Contact Integrated

--- configs/egov-localization/rainmaker-ec.sql
+++ configs/egov-localization/rainmaker-ec.sql
@@ -1,10 +1,13 @@
 DELETE FROM public.message WHERE code = 'EC_SEND_NOTIFICATION' and module='rainmaker-ec' and tenantId='ch';
 DELETE FROM public.message WHERE code = 'EC_SEND_MESSAGE_BUTTON' and module='rainmaker-ec' and tenantId='ch';
-
 DELETE FROM public.message WHERE code = 'EC_MARK_AS_PAID_BUTTON' and module='rainmaker-ec' and tenantId='ch';
 DELETE FROM public.message WHERE code = 'EC_MARK_AS_PAID_SUCCESS' and module='rainmaker-ec' and tenantId='ch';
 DELETE FROM public.message WHERE code = 'EC_DELETE_CHALLAN_BUTTON' and module='rainmaker-ec' and tenantId='ch';
 DELETE FROM public.message WHERE code = 'EC_DELETE_CHALLAN_SUCCESS' and module='rainmaker-ec' and tenantId='ch';
+DELETE FROM public.message WHERE code = 'EC_SM_UPDATE_CONTACT_POPUP_HEADER' and module='rainmaker-ec' and tenantId='ch';
+DELETE FROM public.message WHERE code = 'EC_POPUP_SM_UPDATE_CONTACT_LABEL' and module='rainmaker-ec' and tenantId='ch';
+DELETE FROM public.message WHERE code = 'EC_POPUP_SM_UPDATE_CONTACT_PLACEHOLDER' and module='rainmaker-ec' and tenantId='ch';
+DELETE FROM public.message WHERE code = 'EC_UPDATE_CHALLAN_SUCCESS' and module='rainmaker-ec' and tenantId='ch';
 
 INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
 ( '52e17862-dbd8-4ec6-95f6-382ed3e00001','en_IN', 'EC_SEND_NOTIFICATION','Notification Sent Successfully !','ch','rainmaker-ec', '94');
@@ -18,3 +21,12 @@ INSERT INTO public.message(id, locale, code, message, tenantid, module, createdb
 ( '52e17862-dbd8-4ec6-95f6-382ec3e00005','en_IN', 'EC_DELETE_CHALLAN_BUTTON','Delete Challan','ch','rainmaker-ec', '94');
 INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
 ( '52e17862-dbd8-4ec6-95f6-382ec3e00006','en_IN', 'EC_DELETE_CHALLAN_SUCCESS','Challan Deleted','ch','rainmaker-ec', '94');
+INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
+( '52e17862-dbd8-4ec6-95f6-382ec3e00007','en_IN', 'EC_SM_UPDATE_CONTACT_POPUP_HEADER','Update Contact','ch','rainmaker-ec', '94');
+INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
+( '52e17862-dbd8-4ec6-95f6-382ec3e00008','en_IN', 'EC_POPUP_SM_UPDATE_CONTACT_LABEL','Contact','ch','rainmaker-ec', '94');
+INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
+( '52e17862-dbd8-4ec6-95f6-382ec3e00009','en_IN', 'EC_POPUP_SM_UPDATE_CONTACT_PLACEHOLDER','Enter Contact','ch','rainmaker-ec', '94');
+INSERT INTO public.message(id, locale, code, message, tenantid, module, createdby) VALUES
+( '52e17862-dbd8-4ec6-95f6-382ec3e00010','en_IN', 'EC_UPDATE_CHALLAN_SUCCESS','Contact Updated Sucessfully.','ch','rainmaker-ec', '94');
+
