ad7f1372f02250d68a81431c414669ba3a5a4a0f
cscl-git <57174349+cscl-git@users.noreply.github.com>
2021-04-05 16:49:53 +0530

    Update rainmaker-nulm-localization_common.sql

--- configs/egov-localization/rainmaker-nulm-localization_common.sql
+++ configs/egov-localization/rainmaker-nulm-localization_common.sql
@@ -4,6 +4,6 @@ DELETE FROM public.message WHERE code = 'WF_NULM_Forwarded To JA' and module='ra
 
 INSERT INTO public.message
 (id, locale, code, message, tenantid, "module", createdby, createddate, lastmodifiedby, lastmodifieddate)VALUES
-('4d3b6355-1nul-0001-ac6f-2bc063b66134e', 'en_IN', 'WF_NULM_Forwarded To SDO', 'Forward To SDO', 'ch', 'rainmaker-nulm', 24226, '2020-08-19 16:05:18.445', NULL, NULL)
-('4d3b6355-1nul-0002-ac6f-2bc063b66134e', 'en_IN', 'WF_NULM_Forwarded To ACMC', 'Forward To ACMC', 'ch', 'rainmaker-nulm', 24226, '2020-08-19 16:05:18.445', NULL, NULL)
+('4d3b6355-1nul-0001-ac6f-2bc063b66134e', 'en_IN', 'WF_NULM_Forwarded To SDO', 'Forward To SDO', 'ch', 'rainmaker-nulm', 24226, '2020-08-19 16:05:18.445', NULL, NULL),
+('4d3b6355-1nul-0002-ac6f-2bc063b66134e', 'en_IN', 'WF_NULM_Forwarded To ACMC', 'Forward To ACMC', 'ch', 'rainmaker-nulm', 24226, '2020-08-19 16:05:18.445', NULL, NULL),
 ('4d3b6355-1nul-0003-ac6f-2bc063b66134e', 'en_IN', 'WF_NULM_Forwarded To JA', 'Forward To JA', 'ch', 'rainmaker-nulm', 24226, '2020-08-19 16:05:18.445', NULL, NULL);
